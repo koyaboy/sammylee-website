fd02eaef16a1cbe8743d35bb52782e7397cfcc00
Koya <44787464+koyaboy@users.noreply.github.com>
2024-03-10 16:04:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-red-500 font-bold text-3xl font-poppins">Sammy LEE</div>
+
+      <p className="font-rubik">TEST</p>
+    </>
+  )
+}
+
+export default App
