b516c1d925808143a988aabbdbafd656890c403d
Koya <44787464+koyaboy@users.noreply.github.com>
2024-03-19 19:38:07 +0100

    finish mobile design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
 
-    function openNavigation() {
+    function toggleNavigation() {
         setIsOpen(prevIsOpen => !prevIsOpen)
     }
     return (
@@ -15,7 +15,9 @@ const Header = () => {
 
             {isOpen && (
                 <div className='bg-[#333333] w-[60vw] min-h-screen fixed z-[999] right-0 px-3'>
-                    {/* PUT CLOSE BUTTON HERE */}
+                    <button onClick={() => toggleNavigation()} className='absolute right-4 top-4'>
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="20px" height="20px"><g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{ mixBlendMode: 'normal' }}><g transform="scale(8.53333,8.53333)"><path d="M7,4c-0.25587,0 -0.51203,0.09747 -0.70703,0.29297l-2,2c-0.391,0.391 -0.391,1.02406 0,1.41406l7.29297,7.29297l-7.29297,7.29297c-0.391,0.391 -0.391,1.02406 0,1.41406l2,2c0.391,0.391 1.02406,0.391 1.41406,0l7.29297,-7.29297l7.29297,7.29297c0.39,0.391 1.02406,0.391 1.41406,0l2,-2c0.391,-0.391 0.391,-1.02406 0,-1.41406l-7.29297,-7.29297l7.29297,-7.29297c0.391,-0.39 0.391,-1.02406 0,-1.41406l-2,-2c-0.391,-0.391 -1.02406,-0.391 -1.41406,0l-7.29297,7.29297l-7.29297,-7.29297c-0.1955,-0.1955 -0.45116,-0.29297 -0.70703,-0.29297z"></path></g></g></svg>
+                    </button>
 
                     <nav className='mt-12'>
                         <ul className='text-[#F0EAD6] flex flex-col gap-4 font-rubik text-[1rem] leading-[120%]'>
@@ -35,7 +37,7 @@ const Header = () => {
                         <img src="src/assets/logo.png" alt="SL Logo"></img>
                     </div>
 
-                    <button onClick={() => openNavigation()}>
+                    <button onClick={() => toggleNavigation()}>
                         <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16" fill="none">
                             <rect width="16" height="16" fill="url(#pattern0)" />
                             <defs>
